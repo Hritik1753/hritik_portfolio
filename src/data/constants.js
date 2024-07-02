8f9527bfa2aa697d8b805203fcf7ec8ca2e39fe4
hritik <jnvbbkhritikpaswan@gmail.com>
2024-07-02 17:16:53 +0530

    second commit

--- src/data/constants.js
+++ src/data/constants.js
@@ -145,16 +145,7 @@ export const skills = [
         image:
           "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
       },
-      // {
-      //   name: "Docker",
-      //   image:
-      //     "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
-      // },
-      // {
-      //   name: "Netlify",
-      //   image:
-      //     "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
-      // },
+     
       {
         name: "VS Code",
         image:
@@ -165,16 +156,7 @@ export const skills = [
         image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoYuyhvRzBemw733LQO5MREh9v27f-LtYv9g&s",
       },
-      // {
-      //   name: "Adobe XD",
-      //   image:
-      //     "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
-      // },
-      // {
-      //   name: "Figma",
-      //   image:
-      //     "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
-      // },
+     
     ],
   },
 ];
@@ -198,87 +180,6 @@ export const skills = [
     },
 
 
-    // {
-    //   id: 0,
-    //   img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
-    //   role: "Frontend Engineer Intern",
-    //   company: "Neurobit",
-    //   date: "June 2023 - Present",
-    //   desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
-    //   skills: [
-    //     "ReactJS",
-    //     "Redux",
-    //     "NodeJs",
-    //     "Material UI",
-    //     "HTML",
-    //     "CSS",
-    //     "JavaScript",
-    //   ],
-    //   doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
-    // },
-    // {
-    //   id: 1,
-    //   img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
-    //   role: "Fullstack Externship",
-    //   company: "Flipr",
-    //   date: "June 2023 - July 2023",
-    //   desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
-    //   skills: [
-    //     "ReactJS",
-    //     "Redux",
-    //     "NodeJs",
-    //     "Material UI",
-    //     "HTML",
-    //     "CSS",
-    //     "JavaScript",
-    //     "Docker",
-    //     "AWS",
-    //     "MongoDB",
-    //   ],
-    // },
-    // {
-    //   id: 2,
-    //   img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
-    //   role: "Open Source Contributor ",
-    //   company: "GirlScript Summer of Code",
-    //   date: "May 2023 - Present",
-    //   desc: "Contributing to different open-source projects and learn from industry experts",
-    //   doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
-    // },
-    // {
-    //   id: 3,
-    //   img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/gdsc.jpeg?alt=media&token=c162329c-efaa-4be8-a173-8d3f4c48ea70",
-    //   role: "Android Developer",
-    //   company: "DSC KIIT",
-    //   date: "Nov2021 - Present",
-    //   desc: "As an Android developer at the Google Developers Student Club (GDCS), I have had the opportunity to work on exciting projects and collaborate with talented developers who share my passion for technology. Through my involvement with GDCS, I have also had the opportunity to host and participate in numerous events, including hackathons, study jams, and workshops.",
-    //   skills: [
-    //     "Leadership",
-    //     "Mobile Application Development",
-    //     "Kotlin",
-    //     "XML",
-    //     "Figma",
-    //   ],
-    // },
-    // {
-    //   id: 4,
-    //   img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Rudraksha.jpeg?alt=media&token=8f83f41e-d0a1-486f-9c7f-dd4cd1d09e8d",
-    //   role: "Android Developer Intern",
-    //   company: "Rudraksha Welffare Foundation",
-    //   date: "June 2021 - Oct 2021",
-    //   desc: "As an Android Developer Intern at Rudraksha Welfare Foundation from June 2021 to October 2021, I gained valuable hands-on experience in application development. During my tenure, I contributed to the development of the application's back-end cloud functions using Firebase and implemented a front-end with the MVVM structure, which was designed using Figma. I also added a one-to-one video call feature, conceptualized new features, and optimized app performance. This internship helped me enhance my skills in Android development and gave me an opportunity to work in a collaborative team environment.",
-    //   skills: [
-    //     "Android",
-    //     "Java",
-    //     "Kotlin",
-    //     "XML",
-    //     "Node Js",
-    //     "Cloud Firestore",
-    //     "Firebase",
-    //     "Figma",
-    //   ],
-    //   doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/WhatsApp%20Image%202023-05-05%20at%2012.07.39%20AM.jpeg?alt=media&token=9f0e1648-568b-422d-bd0b-1f125f722245",
-    // },
   ];
 
 export const education = [
@@ -311,31 +212,7 @@ export const education = [
 ];
 
 export const projects = [
-  // {
-  //   id: 9,
-  //   title: "Trackify",
-  //   date: "Jun 2023 - Jul 2023",
-  //   description:
-  //     "Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. \nAdmin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials:	#Email: testemployee@gmail.com	#Password- 123@Testemployee",
-  //   image:
-  //     "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
-  //   tags: [
-  //     "Docker",
-  //     "AWS",
-  //     "DuckDNS",
-  //     "Eslint",
-  //     "Husky",
-  //     "CI/CD",
-  //     "React Js",
-  //     "MongoDb",
-  //     "Node Js",
-  //     "Express Js",
-  //     "Redux",
-  //   ],
-  //   category: "web app",
-  //   github: "https://github.com/rishavchanda/Trackify",
-  //   webapp: "https://trackify.duckdns.org",
-  // },
+
   {
     id: 0,
     title: "real-time-chat-application",
@@ -351,17 +228,12 @@ export const projects = [
     member: [
       {
         name: "Hritik Paswan",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ718nztPNJfCbDJjZG8fOkejBnBAeQw5eAUA&s",
+        img: "https://media.licdn.com/dms/image/D4D03AQEgjHH3TsvWhQ/profile-displayphoto-shrink_800_800/0/1699725247520?e=1723680000&v=beta&t=iesyd3qQIpX7Q_fbcEf5ZSo6V0ptssg_kXNXqznqhr0",
         linkedin: "https://www.linkedin.com/in/hritik-paswan/",
         github: "https://github.com/hritik9335502064",
       },
      
-      // {
-      //   name: "Upasana Chaudhuri",
-      //   img: "https://avatars.githubusercontent.com/u/100614635?v=4",
-      //   linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
-      //   github: "https://github.com/upasana0710",
-      // },
+      
     ],
   },
   {
@@ -382,7 +254,17 @@ export const projects = [
     ],
     category: "web app",
     linkedin: "https://www.linkedin.com/in/hritik-paswan/",
+    github: "https://github.com/hritik9335502064",
+    member: [
+      {
+        name: "Hritik Paswan",
+        img: "https://media.licdn.com/dms/image/D4D03AQEgjHH3TsvWhQ/profile-displayphoto-shrink_800_800/0/1699725247520?e=1723680000&v=beta&t=iesyd3qQIpX7Q_fbcEf5ZSo6V0ptssg_kXNXqznqhr0",
+        linkedin: "https://www.linkedin.com/in/hritik-paswan/",
         github: "https://github.com/hritik9335502064",
+      },
+     
+     
+    ],
   },
   {
     id: 2,
@@ -403,12 +285,7 @@ export const projects = [
         linkedin: "https://www.linkedin.com/in/hritik-paswan/",
         github: "https://github.com/hritik9335502064",
       },
-      // {
-      //   name: "Upasana Chaudhuri",
-      //   img: "https://avatars.githubusercontent.com/u/100614635?v=4",
-      //   linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
-      //   github: "https://github.com/upasana0710",
-      // },
+    
     ],
   },
   {
@@ -436,12 +313,7 @@ export const projects = [
         linkedin: "https://www.linkedin.com/in/sejalraj2002/",
         github: "https://github.com/raj-sejal",
       },
-      // {
-      //   name: "Upasana Chaudhuri",
-      //   img: "https://avatars.githubusercontent.com/u/100614635?v=4",
-      //   linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
-      //   github: "https://github.com/upasana0710",
-      // },
+     
     ],
   },
   {
@@ -457,6 +329,37 @@ export const projects = [
     linkedin: "https://www.linkedin.com/in/hritik-paswan/",
     github: " Decentralized-e-Voting-System",
     webapp: " ",
+    member: [
+      {
+        name: "Hritik Paswan",
+        img: "https://media.licdn.com/dms/image/D4D03AQEgjHH3TsvWhQ/profile-displayphoto-shrink_800_800/0/1699725247520?e=1723680000&v=beta&t=iesyd3qQIpX7Q_fbcEf5ZSo6V0ptssg_kXNXqznqhr0",
+        linkedin: "https://www.linkedin.com/in/hritik-paswan/",
+        github: "https://github.com/hritik9335502064",
+      },
+     
+      {
+        name: "Arpit Kumar mishra",
+        img: "https://media.licdn.com/dms/image/D5603AQE6ajI5xSR8Ew/profile-displayphoto-shrink_800_800/0/1713714121952?e=1725494400&v=beta&t=lKWfs1W8tEXFjcNVGUdsVzK8kGLr6Ktj2Hb5Y-hF2Ig",
+        linkedin: "https://www.linkedin.com/in/arpitkumarmishra07/ ",
+        github: "",
+      },
+
+      {
+        name: "Jaydeep Murtee",
+        img: "https://media.licdn.com/dms/image/D4D03AQH28iRJuqbnrQ/profile-displayphoto-shrink_800_800/0/1685465818080?e=1725494400&v=beta&t=O__qNVNDN4iEVhE9Kd4DXG3m9vxR0z6dytOQH5D-fU8",
+        linkedin: "https://www.linkedin.com/in/jaideep-murthy/",
+        github: "",
+      },
+
+      {
+        name: "Ayush kumar nishod",
+        img: "https://media.licdn.com/dms/image/D5603AQFdkMY_ACk-9w/profile-displayphoto-shrink_800_800/0/1719212219619?e=1725494400&v=beta&t=zJOFdZFjcm16pOAESkOdGGhdwx4c0dmuq9saSNsA-vc",
+        linkedin: "https://www.linkedin.com/in/nishod/",
+        github: "",
+      },
+
+    ],
+
   },
   {
     id: 4,
@@ -470,6 +373,16 @@ export const projects = [
     category: "web app",
     github: "https://github.com/Hritik1753?tab=repositories",
     webapp: "",
+    member: [
+      {
+        name: "Hritik Paswan",
+        img: "https://media.licdn.com/dms/image/D4D03AQEgjHH3TsvWhQ/profile-displayphoto-shrink_800_800/0/1699725247520?e=1723680000&v=beta&t=iesyd3qQIpX7Q_fbcEf5ZSo6V0ptssg_kXNXqznqhr0",
+        linkedin: "https://www.linkedin.com/in/hritik-paswan/",
+        github: "https://github.com/hritik9335502064",
+      },
+     
+      
+    ],
   },
   {
     id: 5,
@@ -481,8 +394,23 @@ export const projects = [
       "todolist.png",
     tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
     category: "web app",
-    github: "https://github.com/rishavchanda/Todo-Web-App",
-    webapp: "https://rishav-react-todo.netlify.app/",
+    github: "",
+    webapp: "",
+    member: [
+      {
+        name: "Hritik Paswan",
+        img: "https://media.licdn.com/dms/image/D4D03AQEgjHH3TsvWhQ/profile-displayphoto-shrink_800_800/0/1699725247520?e=1723680000&v=beta&t=iesyd3qQIpX7Q_fbcEf5ZSo6V0ptssg_kXNXqznqhr0",
+        linkedin: "https://www.linkedin.com/in/hritik-paswan/",
+        github: "https://github.com/hritik9335502064",
+      },
+     
+      // {
+      //   name: "Upasana Chaudhuri",
+      //   img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+      //   linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+      //   github: "https://github.com/upasana0710",
+      // },
+    ],
   },
   {
     id: 6,
@@ -496,6 +424,22 @@ export const projects = [
     category: "web app",
     github: "https://github.com/Hritik1753?tab=repositories",
     webapp: "",
+
+    member: [
+      {
+        name: "Hritik Paswan",
+        img: "https://media.licdn.com/dms/image/D4D03AQEgjHH3TsvWhQ/profile-displayphoto-shrink_800_800/0/1699725247520?e=1723680000&v=beta&t=iesyd3qQIpX7Q_fbcEf5ZSo6V0ptssg_kXNXqznqhr0",
+        linkedin: "https://www.linkedin.com/in/hritik-paswan/",
+        github: "https://github.com/hritik9335502064",
+      },
+     
+      // {
+      //   name: "Upasana Chaudhuri",
+      //   img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+      //   linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+      //   github: "https://github.com/upasana0710",
+      // },
+    ],
   },
   // {
   //   id: 7,
